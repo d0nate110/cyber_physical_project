e5b5276b036a62ce48ca11e0e34a50a823d76c2b
askan <askan@student.chalmers.se>
2023-05-04 16:57:35 +0200

    feat!!!: Adding multiple tests, and the logic for CSV IO Helps #31 Helps #32 Helps #17 Helps #16

--- src/TestPrimeChecker.cpp
+++ src/TestPrimeChecker.cpp
@@ -1,28 +1,178 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "PrimeChecker.hpp"
+#include "data_handler/data_handler.hpp"
+#include "test/performance_tests.hpp"
+using namespace std;
 
-TEST_CASE("Test Primechecker 1.") {
-    PrimeChecker pc;
-    REQUIRE(pc.isPrime(5));
+
+//THESE TESTS SHOW THE ACCURACY OF THE ALGO
+TEST_CASE("Test Algorithm Accuracy - File 1") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_accuracy(dataValue, outputContent);
+    REQUIRE(accuracy >= 40);
 }
 
-TEST_CASE("Test PrimeChecker 2.") {
-    PrimeChecker pc;
-    REQUIRE_FALSE(pc.isPrime(1)); 
+TEST_CASE("Test Algorithm Accuracy - File 2") {
+    string filePath = "../data/Data2_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output2_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_accuracy(dataValue, outputContent);
+    REQUIRE(accuracy >= 40);
 }
 
-TEST_CASE("Test PrimeChecker 3.") {
-    PrimeChecker pc;
-    REQUIRE_FALSE(pc.isPrime(4)); 
+TEST_CASE("Test Algorithm Accuracy - File 3") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output3_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_accuracy(dataValue, outputContent);
+    REQUIRE(accuracy >= 40);
 }
 
-TEST_CASE("Test PrimeChecker 4.") {
-    PrimeChecker pc;
-    REQUIRE_FALSE(pc.isPrime(-5)); 
+
+TEST_CASE("Test Algorithm Accuracy - File 4") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output4_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_accuracy(dataValue, outputContent);
+    REQUIRE(accuracy >= 40);
 }
 
-TEST_CASE("Test PrimeChecker 5.") {
-    PrimeChecker pc;
-    REQUIRE_FALSE(pc.isPrime(0)); 
+TEST_CASE("Test Algorithm Accuracy - File 5") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output5_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_accuracy(dataValue, outputContent);
+    REQUIRE(accuracy >= 40);
+}
+
+
+//THESE ARE TESTS FOR TIME TAKEN COMPARED TO DATA
+TEST_CASE("Test Algorithm Time - File 1") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    pair<int, int> accuracy = performance_tests::algorithm_performance_time(dataValue, outputContent);
+    REQUIRE(accuracy.first == accuracy.second);
+}
+
+TEST_CASE("Test Algorithm Time - File 2") {
+    string filePath = "../data/Data2_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output2_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    pair<int, int> accuracy = performance_tests::algorithm_performance_time(dataValue, outputContent);
+    REQUIRE(accuracy.first == accuracy.second);
+}
+
+TEST_CASE("Test Algorithm Time - File 3") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output3_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    pair<int, int> accuracy = performance_tests::algorithm_performance_time(dataValue, outputContent);
+    REQUIRE(accuracy.first == accuracy.second);
+}
+
+TEST_CASE("Test Algorithm Time - File 4") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output4_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    pair<int, int> accuracy = performance_tests::algorithm_performance_time(dataValue, outputContent);
+    REQUIRE(accuracy.first == accuracy.second);
+}
+
+TEST_CASE("Test Algorithm Time - File 5") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output5_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    pair<int, int> accuracy = performance_tests::algorithm_performance_time(dataValue, outputContent);
+    REQUIRE(accuracy.first == accuracy.second);
+}
+
+
+//THESE TESTS CHECKS TO SEE IF SECOND HAS 10 FRAMES
+TEST_CASE("Test Algorithm Frame per Second - File 1") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_performance_frame(dataValue, outputContent);
+    REQUIRE(accuracy >= 85);
+}
+
+TEST_CASE("Test Algorithm Frame per Second - File 2") {
+    string filePath = "../data/Data2_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output2_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_performance_frame(dataValue, outputContent);
+    REQUIRE(accuracy >= 85);
+}
+
+TEST_CASE("Test Algorithm Frame per Second - File 3") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output3_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_performance_frame(dataValue, outputContent);
+    REQUIRE(accuracy >= 85);
+}
+
+
+TEST_CASE("Test Algorithm Frame per Second - File 4") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output4_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_performance_frame(dataValue, outputContent);
+    REQUIRE(accuracy >= 85);
+}
+
+TEST_CASE("Test Algorithm Frame per Second - File 5") {
+    string filePath = "../data/Data1_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> dataValue = data_handler::read_csv_file(filePath);
+
+    string newPath = "../algorithm_output/Output5_SteeringAngle.csv";
+    vector<pair<unsigned long long int, double>> outputContent = data_handler::read_csv_file(newPath);
+
+    double accuracy = performance_tests::algorithm_performance_frame(dataValue, outputContent);
+    REQUIRE(accuracy >= 85);
 }
