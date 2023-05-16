8ae86ac0b536fb0aab30c3c3c1ea6625a9384403
askan <askan@student.chalmers.se>
2023-05-16 23:00:15 +0200

    2ndImplementation: the 2nd version with weights but not inversed

--- src/algorithm/algorithm.cpp
+++ src/algorithm/algorithm.cpp
@@ -10,14 +10,22 @@
 #define MAX_OPENCV_DISTANCE 268.365
 #define MIN_OPENCV_DISTANCE 0
 
+#define HIGH_PRIORITY 0.75
+#define MIDDLE_PRIORITY 0.5
+#define LOW_PRIORITY 0.25
+
+//Average of turn before another one as the difference
 #define AVERAGE_DIFFERENCE 0.00283774
 
 double calculateSteeringWheelAngle(double coneAngle, double coneDistance) {
 
+    float angleWeight = MIDDLE_PRIORITY;
+    float distanceWeight = MIDDLE_PRIORITY;
+
     float normalizedDistance = coneDistance / MAX_OPENCV_DISTANCE;
     float normalizedAngle = coneAngle / MAX_CONE_ANGLE;
 
-    float steeringAngle = (normalizedDistance / 2 + normalizedAngle / 2) * MAX_STEERING_WHEEL_ANGLE;
+    float steeringAngle = (normalizedDistance * distanceWeight + normalizedAngle * angleWeight) * MAX_STEERING_WHEEL_ANGLE;
 
     return steeringAngle;
 }
