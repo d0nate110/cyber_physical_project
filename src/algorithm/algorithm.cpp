dc4698bafa2cf4c7a00701e623c54d8d8eca744f
Anton Golubenko <anton@student.chalmers.se>
2023-05-18 14:25:03 +0200

    feat: take away abs if block

--- src/algorithm/algorithm.cpp
+++ src/algorithm/algorithm.cpp
@@ -9,16 +9,11 @@ double calculateSteeringWheelAngle(double angularVelocityZ)
    // multiply angularVelocity value by 0.003, and assign it to steeringAngle variable.
    steeringAngle = angularVelocityZ * 0.003;
 
-   // if the absolute value of steeringAngle (- or +) is within the range of 0.03, then set the steeringAngle variable to 0. 
-   if (std::abs(steeringAngle) <= 0.03)
-   {
-       steeringAngle = 0;
-   }
    // if the steeringAngle variable bigger than or equal to 0.2, then set the variable to 0.240888. Because the max number in a track is 0.290888. 
    if (steeringAngle >= 0.2) {
-    steeringAngle = 0.240888;
+    steeringAngle = 0.21;
    }  else if (steeringAngle <= -0.2) { // On the contrary, if the steeringAngle variable is less than -0.2, then set the variable to -0.290888.
-    steeringAngle = -0.240888;
+    steeringAngle = -0.21;
 
    }
 
